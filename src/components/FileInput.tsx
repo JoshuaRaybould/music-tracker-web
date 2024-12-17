b8509e4547e52f5beb803dff2de1065789d6eae5
JoshuaRaybould <joshua.raybould@outlook.com>
2024-12-17 00:13:29 +0000

    Update to iterate through files

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -1,10 +1,14 @@
 function FileInput() {
-   const onChange = (
-      event: React.ChangeEvent<HTMLInputElement | HTMLAreaElement>
-   ) => {
-      const files = (event.target as HTMLInputElement).files;
-
-      console.log(files);
+   const onChange = (e: React.FormEvent<HTMLInputElement>): void => {
+      let files: FileList | null = e.currentTarget.files;
+      if (files) {
+         // Check if files is not null
+         Array.from(files).forEach((file) => {
+            console.log("Do something with " + file.name);
+         });
+      } else {
+         console.log("No files selected");
+      }
    };
 
    return (
