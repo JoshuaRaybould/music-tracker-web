47083cf173baffdc05c718cb13111b153894f6c6
JoshuaRaybould <joshua.raybould@outlook.com>
2024-12-17 20:28:19 +0000

    Add basic song processing
    Adds up time listened for songs, removing duplicates and any null songs.
    Also filters out songs listened to less than 20s.

--- src/processData.ts
+++ src/processData.ts
@@ -12,23 +12,50 @@ function ProcessContents(file : File){
       const contents = event?.target?.result as string;
       const jsonData = JSON.parse(contents);
 
-      const songs: Song[] = jsonData.map((data: any) => CleanSongData(data));
-      console.log(songs[0].uri);
+      const songs: Song[] = jsonData.map((data: any) => DataToSong(data));
+
+      const cleanSongs = CleanSongs(songs);
+      for (var i = 0; i < 50; i++) {
+         console.log(cleanSongs[i].uri);
+         console.log(cleanSongs[i].name);
+         console.log(cleanSongs[i].timeListened);
+      }
       
       // do something with the file contents here
    };
    
-   const conts = fileReader.readAsText(file)
+   fileReader.readAsText(file)
 }
 
-function CleanSongData(data : any) {
+function DataToSong(data : any) {
    return {
       uri: data.spotify_track_uri,
       name: data.master_metadata_track_name,
       artist: data.master_metadata_album_artist_name,
       album: data.master_metadata_album_album_name,
-      timeListened: data.ms_played
+      timeListened: (+data.ms_played)/1000
    }
+}
+
+function CleanSongs(songs : Song[]) : Song[] {
+   const cleanSongs : Song[] = [];
+   for (const song of songs) {
+      if (song.name === null || song.timeListened == null || song.timeListened < 20) {
+         continue;
+      }
+      var songFound = false;
+      for (const cleanedSong of cleanSongs) {
+         if (cleanedSong.uri === song.uri) {
+            songFound = true;
+            cleanedSong.timeListened = cleanedSong.timeListened +song.timeListened;
+            break;
+         }
+      }
+      if (!songFound) {
+         cleanSongs.push(song);
+      }
+   }
+   return cleanSongs;
 }
 
 export default ProcessContents
