cb8a3bff31cdf5749b4ccb86a28f2c05738528b7
JoshuaRaybould <joshua.raybould@outlook.com>
2024-12-17 19:59:08 +0000

    Update to put music data into songs array

--- src/processData.ts
+++ src/processData.ts
@@ -1,20 +1,34 @@
-/*type Song = {
+type Song = {
    uri: string,
    name: string,
    artist: string,
    album: string,
    timeListened: number
-}*/
+}
 
-function ProcessContents(file : File) {
+function ProcessContents(file : File){
    const fileReader = new FileReader();
    fileReader.onload = (event) => {
-      const contents = event?.target?.result;
-      console.log(contents)
-      return contents;
+      const contents = event?.target?.result as string;
+      const jsonData = JSON.parse(contents);
+
+      const songs: Song[] = jsonData.map((data: any) => CleanSongData(data));
+      console.log(songs[0].uri);
+      
       // do something with the file contents here
    };
-   fileReader.readAsText(file)
+   
+   const conts = fileReader.readAsText(file)
+}
+
+function CleanSongData(data : any) {
+   return {
+      uri: data.spotify_track_uri,
+      name: data.master_metadata_track_name,
+      artist: data.master_metadata_album_artist_name,
+      album: data.master_metadata_album_album_name,
+      timeListened: data.ms_played
+   }
 }
 
 export default ProcessContents
