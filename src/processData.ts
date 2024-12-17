f1d5b5737f1a5aa24d9cde0e7cdf659663d6fb41
JoshuaRaybould <joshua.raybould@outlook.com>
2024-12-17 23:25:01 +0000

    Update processData to return a promise

--- src/processData.ts
+++ src/processData.ts
@@ -1,30 +1,32 @@
-type Song = {
-   uri: string,
-   name: string,
-   artist: string,
-   album: string,
-   timeListened: number
-}
+import { Song } from "./types" 
 
-function ProcessContents(file : File){
-   const fileReader = new FileReader();
-   fileReader.onload = (event) => {
-      const contents = event?.target?.result as string;
-      const jsonData = JSON.parse(contents);
+function ProcessContents(file: File): Promise<Song[]> {
+   return new Promise((resolve, reject) => {
+       const fileReader = new FileReader();
+       
+       fileReader.onload = (event) => {
+           try {
+               const contents = event?.target?.result as string;
+               const jsonData = JSON.parse(contents);
+               const songs: Song[] = jsonData.map((data: any) => DataToSong(data));
+               const cleanSongs = CleanSongs(songs);
+               
+               for (var i = 0; i < 50; i++) {
+                   console.log(cleanSongs[i].uri);
+                   console.log(cleanSongs[i].name);
+                   console.log(cleanSongs[i].timeListened);
+               }
+               
+               resolve(cleanSongs);
+           } catch (error) {
+               reject(error);
+           }
+       };
 
-      const songs: Song[] = jsonData.map((data: any) => DataToSong(data));
-
-      const cleanSongs = CleanSongs(songs);
-      for (var i = 0; i < 50; i++) {
-         console.log(cleanSongs[i].uri);
-         console.log(cleanSongs[i].name);
-         console.log(cleanSongs[i].timeListened);
-      }
-      
-      // do something with the file contents here
-   };
-   
-   fileReader.readAsText(file)
+       fileReader.onerror = () => reject(fileReader.error);
+       
+       fileReader.readAsText(file);
+   });
 }
 
 function DataToSong(data : any) {
