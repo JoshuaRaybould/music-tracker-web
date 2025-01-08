a6db9d4fc5ea9c977fea5a24ccb31d9d7e202729
JoshuaRaybould <joshua.raybould@outlook.com>
2025-01-08 22:16:50 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import FileInput from "./components/FileInput";
 import { Song } from "./types";
 import BorderedTable from "./components/BorderedTable";
+import ArtistBorderedTable from "./components/ArtistBorderedTable";
 import songsToAlbums from "./albumLogic";
 
 function App() {
@@ -13,14 +14,29 @@ function App() {
       });
    }
 
+   const albums = songsToAlbums(songs);
+   const artists = songsToAlbums(songs);
+
    return (
       <div>
          <FileInput addSongs={addSongs} />
-         <BorderedTable items={songs} firstColTitle={"Song"}></BorderedTable>
          <BorderedTable
-            items={songsToAlbums(songs)}
-            firstColTitle={"Album"}
+            items={songs}
+            firstCol={"Song"}
+            secondCol={"Artist"}
+            thirdCol={"Time Listened"}
          ></BorderedTable>
+         <BorderedTable
+            items={albums}
+            firstCol={"Album"}
+            secondCol={"Artist"}
+            thirdCol={"Time Listened"}
+         ></BorderedTable>
+         <ArtistBorderedTable
+            items={artists}
+            firstCol={"Artist"}
+            secondCol={"Time Listened"}
+         ></ArtistBorderedTable>
       </div>
    );
 }
