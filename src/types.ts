cf617f50d4975ad051fa442cc39333bd6b6efc4c
JoshuaRaybould <joshua.raybould@outlook.com>
2025-01-09 16:26:10 +0000

    Update types

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,10 @@ type Song = {
    name: string,
    artist: string,
    album: string,
-   timeListened: number
+   timeListened: number,
+   plays: number,
+   uniqueVal: number, // streak, max amount of times it was listened to in a row
+   firstListened: EpochTimeStamp
 }
 
 export type { Song }
@@ -11,14 +14,20 @@ export type { Song }
 type Album = {
    name: string,
    artist: string,
-   timeListened: number
+   timeListened: number,
+   plays: number,
+   uniqueVal: number, // idk what this will be if anything, just place holder here
+   firstListened: EpochTimeStamp
 }
 
 export type { Album }
 
 type Artist = {
    name: string,
-   timeListened: number
+   timeListened: number,
+   plays: number,
+   streak: number,
+   firstListened: EpochTimeStamp
 }
 
 export type { Artist }
